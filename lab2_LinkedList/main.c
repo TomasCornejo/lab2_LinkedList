080ddaec3678638dd31d0e74626956f0e19345d1
Tomas <tcornejo@fen.uchile.cl>
2022-06-08 23:02:46 -0400

    Initial Commit

---
+++ lab2_LinkedList/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  lab2_LinkedList
+//
+//  Created by Tomas Cornejo on 08-06-22.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
